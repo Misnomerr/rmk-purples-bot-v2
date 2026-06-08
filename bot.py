71fd229bb357017464699f9655b475790cc9e719
Misnomerr <jakeharrypowell@gmail.com>
2026-06-09 00:13:54 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,4 +21,14 @@ async def on_ready():
     print(f"Logged in as {bot.user}")
 
 async def main():
-   
+    setup_database()
+
+    await bot.load_extension("cogs.tickets")
+
+    synced = await bot.tree.sync()
+
+    print(f"Synced {len(synced)} commands")
+
+    await bot.start(TOKEN)
+
+asyncio.run(main())
