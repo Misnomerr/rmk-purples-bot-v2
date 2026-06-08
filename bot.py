71b55e107c63039f6b9e73cad95ab7d70d419b14
Misnomerr <jakeharrypowell@gmail.com>
2026-06-09 00:19:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,36 @@
 import discord
+from discord.ext import commands
+import os
+import asyncio
+
+from database import setup_database
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(f"Sync error: {e}")
+
+async def main():
+    setup_database()
+
+    await bot.load_extension("cogs.tickets")
+
+    await bot.start(TOKEN)
+
+asyncio.run(main())
