5f9616bc14ac8847b19bb14cef34ba1811f5cc6d
Misnomerr <jakeharrypowell@gmail.com>
2026-06-09 00:36:18 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,33 +1,3 @@
-import sqlite3
-from pathlib import Path
-
-DB_PATH = Path("data/database.db")
-DB_PATH.parent.mkdir(exist_ok=True)
-
-def get_connection():
-    return sqlite3.connect(DB_PATH)
-
-def setup_database():
-    conn = get_connection()
-    cur = conn.cursor()
-
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS ticket_counter (
-        category TEXT PRIMARY KEY,
-        current_number INTEGER NOT NULL
-    )
-    """)
-
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS tickets (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        ticket_number TEXT,
-        ticket_type TEXT,
-        creator_id INTEGER,
-        channel_id INTEGER,
-        status TEXT
-    )
-    """)
 
     conn.commit()
     conn.close()
@@ -60,3 +30,30 @@ def get_next_ticket_number(category: str):
     conn.close()
 
     return f"{number:04d}"
+def create_ticket(ticket_number, ticket_type, creator_id, channel_id):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+        INSERT INTO tickets
+        (
+            ticket_number,
+            ticket_type,
+            creator_id,
+            channel_id,
+            status
+        )
+        VALUES (?, ?, ?, ?, ?)
+        """,
+        (
+            ticket_number,
+            ticket_type,
+            creator_id,
+            channel_id,
+            "open"
+        )
+    )
+
+    conn.commit()
+    conn.close()
