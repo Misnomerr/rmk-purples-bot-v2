0e783f02e9445c77225f3aad1493289dcf2e19d4
Misnomerr <jakeharrypowell@gmail.com>
2026-06-08 23:59:05 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
+import discord
+from discord.ext import commands
+import os
 
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    print("RMK Purples V2 Online")
+
+try:
+   
