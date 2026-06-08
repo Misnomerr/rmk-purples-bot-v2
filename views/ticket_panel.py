0291e80c53d69b51a9d8dfdddac2f07ea53b32b5
Misnomerr <jakeharrypowell@gmail.com>
2026-06-09 00:41:24 +0100

    Update ticket_panel.py

--- views/ticket_panel.py
+++ views/ticket_panel.py
@@ -6,6 +6,7 @@ from database import (
     create_ticket
 )
 
+
 class TicketTypeSelect(discord.ui.Select):
     def __init__(self):
 
@@ -34,77 +35,77 @@ class TicketTypeSelect(discord.ui.Select):
             options=options
         )
 
-  async def callback(self, interaction: discord.Interaction):
+    async def callback(self, interaction: discord.Interaction):
 
-    guild = interaction.guild
+        guild = interaction.guild
 
-    ticket_type = self.values[0]
+        ticket_type = self.values[0]
 
-    number = get_next_ticket_number(ticket_type)
+        number = get_next_ticket_number(ticket_type)
 
-    channel_name = f"{ticket_type}-{number}"
+        channel_name = f"{ticket_type}-{number}"
 
-    category = guild.get_channel(
-        config.TICKETS_CATEGORY_ID
-    )
-
-    staff_role = guild.get_role(
-        config.STAFF_ROLE_ID
-    )
-
-    overwrites = {
-        guild.default_role: discord.PermissionOverwrite(
-            view_channel=False
-        ),
-
-        interaction.user: discord.PermissionOverwrite(
-            view_channel=True,
-            send_messages=True
-        ),
-
-        staff_role: discord.PermissionOverwrite(
-            view_channel=True,
-            send_messages=True
-        ),
-
-        guild.me: discord.PermissionOverwrite(
-            view_channel=True,
-            send_messages=True
+        category = guild.get_channel(
+            config.TICKETS_CATEGORY_ID
         )
-    }
 
-    channel = await guild.create_text_channel(
-        name=channel_name,
-        category=category,
-        overwrites=overwrites
-    )
+        staff_role = guild.get_role(
+            config.STAFF_ROLE_ID
+        )
 
-    create_ticket(
-        number,
-        ticket_type,
-        interaction.user.id,
-        channel.id
-    )
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(
+                view_channel=False
+            ),
 
-    embed = discord.Embed(
-        title="Ticket Created",
-        description=(
-            f"Type: {ticket_type}\n"
-            f"Number: {number}"
-        ),
-        color=0x8000ff
-    )
+            interaction.user: discord.PermissionOverwrite(
+                view_channel=True,
+                send_messages=True
+            ),
 
-    await channel.send(
-        f"{interaction.user.mention}"
-    )
+            staff_role: discord.PermissionOverwrite(
+                view_channel=True,
+                send_messages=True
+            ),
 
-    await channel.send(embed=embed)
+            guild.me: discord.PermissionOverwrite(
+                view_channel=True,
+                send_messages=True
+            )
+        }
 
-    await interaction.response.send_message(
-        f"Created {channel.mention}",
-        ephemeral=True
-    )
+        channel = await guild.create_text_channel(
+            name=channel_name,
+            category=category,
+            overwrites=overwrites
+        )
+
+        create_ticket(
+            number,
+            ticket_type,
+            interaction.user.id,
+            channel.id
+        )
+
+        embed = discord.Embed(
+            title="Ticket Created",
+            description=(
+                f"Type: {ticket_type}\n"
+                f"Number: {number}"
+            ),
+            color=0x8000ff
+        )
+
+        await channel.send(
+            f"{interaction.user.mention}"
+        )
+
+        await channel.send(embed=embed)
+
+        await interaction.response.send_message(
+            f"Created {channel.mention}",
+            ephemeral=True
+        )
 
 
 class TicketTypeView(discord.ui.View):
