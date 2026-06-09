aa2addc04de8d1811da7a43cd1edf762a5fe86b3
Misnomerr <jakeharrypowell@gmail.com>
2026-06-09 11:51:45 +0100

    Update feedback.py

--- cogs/feedback.py
+++ cogs/feedback.py
@@ -1,1 +1,31 @@
+import discord
 
+from discord.ext import commands
+from discord import app_commands
+
+from views.feedback_views import FeedbackModal
+
+
+class Feedback(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(
+        name="feedback",
+        description="Submit customer feedback"
+    )
+    async def feedback(
+        self,
+        interaction: discord.Interaction
+    ):
+
+        await interaction.response.send_modal(
+            FeedbackModal()
+        )
+
+
+async def setup(bot):
+    await bot.add_cog(
+        Feedback(bot)
+    )
