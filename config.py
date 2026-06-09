a0792e8f66e443cbb315c1781a566bb0c6737c16
Misnomerr <jakeharrypowell@gmail.com>
2026-06-09 13:52:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ CREATE_TICKET_CHANNEL_ID = int(
     os.getenv("CREATE_TICKET_CHANNEL_ID")
 )
 
-ADMIN_ROLE_ID = int(
-    os.getenv("ADMIN_ROLE_ID")
+OWNER_ROLE_ID = int(
+    os.getenv("OWNER_ROLE_ID")
 )
 
 TICKET_LOGS_CHANNEL_ID = int(
